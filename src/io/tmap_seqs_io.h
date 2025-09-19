9756ebd8a1bf99ba72c58ea2ebe6633f418bd856
John Verran <jverran@epiminder.com>
2025-09-19 18:35:24 +1000

    fix: remove inline keywords from function declarations in io headers
    Remove inline keywords from function declarations in io module header
    files where functions are actually defined in corresponding .c files.
    This fixes linker errors for file I/O related functions.

--- src/io/tmap_seqs_io.h
+++ src/io/tmap_seqs_io.h
@@ -33,14 +33,14 @@ typedef struct {
   @param  compression  the compression type
   @return              pointer to the initialized memory for reading/writing sequences
   */
-inline tmap_seqs_io_t *
+ tmap_seqs_io_t *
 tmap_seqs_io_init(char **fns, int32_t fn_num, int8_t seq_type, int32_t compression);
 
 /*! 
   destroys input/output structure
   @param  io  a pointer to the sequence structure
   */
-inline void
+ void
 tmap_seqs_io_destroy(tmap_seqs_io_t *io);
 
 /*! 
@@ -50,7 +50,7 @@ tmap_seqs_io_destroy(tmap_seqs_io_t *io);
   @param  header  the (output) SAM header
   @return        non-negative if successfull, -1 indicates an a EOF, -2 indicates a truncated quality string
   */
-inline int
+ int
 tmap_seqs_io_read(tmap_seqs_io_t *io, tmap_seqs_t *seqs, sam_header_t *header);
 
 /*! 
